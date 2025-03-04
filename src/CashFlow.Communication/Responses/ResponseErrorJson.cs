2a9139ee1679e3f20ce1737ea9a2fce245618b36
Daniel Sângelo <danielsangelo@hotmail.com>
2025-03-04 15:12:04 -0300

    Add error response files

---
+++ src/CashFlow.Communication/Responses/ResponseErrorJson.cs
@@ -0,0 +1,16 @@
+﻿namespace CashFlow.Communication.Responses;
+
+public class ResponseErrorJson
+{
+    public List<string> ErrorMessage { get; set; } = [];
+
+    public ResponseErrorJson(List<string> errorMessages)
+    {
+        ErrorMessage = errorMessages;
+    }
+
+    public ResponseErrorJson(string errorMessage)
+    {
+        ErrorMessage = [errorMessage];
+    }
+}
