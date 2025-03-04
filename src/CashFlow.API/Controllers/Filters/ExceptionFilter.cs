e7f4243dfabd7199ab02b64c93bfb6c51402dfd9
Daniel Sângelo <danielsangelo@hotmail.com>
2025-03-04 15:12:49 -0300

    Add filter and middleware

---
+++ src/CashFlow.API/Controllers/Filters/ExceptionFilter.cs
@@ -0,0 +1,38 @@
+﻿using CashFlow.Communication.Responses;
+using CashFlow.Exception;
+using CashFlow.Exception.ExceptionBase;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CashFlow.API.Controllers.Filters;
+
+public class ExceptionFilter : IExceptionFilter
+{
+    void IExceptionFilter.OnException(ExceptionContext context)
+    {
+        if (context.Exception is CashFlowException)
+        {
+            HandleProjectException(context);
+        } else
+        {
+            ThrowUnknowError(context);
+        }
+    }
+
+    private void HandleProjectException(ExceptionContext context)
+    {
+        var errorResponse = (context.Exception is ErrorOnValidationException ex)
+            ? new ResponseErrorJson(ex.Errors)
+            : new ResponseErrorJson(context.Exception.Message);
+
+        context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+        context.Result = new BadRequestObjectResult(errorResponse);
+    }
+
+    private void ThrowUnknowError(ExceptionContext context)
+    {
+        var errorResponse = new ResponseErrorJson(ResourceErrorMessages.UNKNOWN_ERROR);
+        context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Result = new ObjectResult(errorResponse);
+    }
+}
---
+++ src/CashFlow.API/Middleware/CultureMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace CashFlow.API.Middleware;
+
+public class CultureMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public CultureMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        var supportedLanguages = CultureInfo.GetCultures(CultureTypes.AllCultures).ToList();
+        var requestedCulture = context.Request.Headers.AcceptLanguage.FirstOrDefault();
+
+        var cultureInfo = (!string.IsNullOrWhiteSpace(requestedCulture) && supportedLanguages.Exists(lang => lang.Name.Equals(requestedCulture)))
+            ? new CultureInfo(requestedCulture)
+            : new CultureInfo("en");
+
+        CultureInfo.CurrentCulture = cultureInfo;
+        CultureInfo.CurrentUICulture = cultureInfo;
+
+        await _next(context);
+    }
+}
