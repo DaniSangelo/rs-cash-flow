a8f990273d5e37c1c56e3119d5c2ffc25e3b0330
Daniel Sângelo <danielsangelo@hotmail.com>
2025-03-04 15:13:43 -0300

    Add register expense use case and controller

---
+++ src/CashFlow.Application/UseCases/Expenses/RegisterExpenseUseCase.cs
@@ -0,0 +1,26 @@
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+using CashFlow.Exception.ExceptionBase;
+using FluentValidation.Results;
+
+namespace CashFlow.Application.UseCases.Expenses;
+
+public class RegisterExpenseUseCase
+{
+    public ResponseRegisterExpenseJson Execute(RequestRegisterExpenseJson request)
+    {
+        Validate(request);
+        return new ResponseRegisterExpenseJson();
+    }
+
+    private void Validate(RequestRegisterExpenseJson request)
+    {
+        var validatorResult = new RegisterExpenseValidator().Validate(request);
+        if (!validatorResult.IsValid)
+        {
+            var errorMessages = validatorResult.Errors.Select(error => error.ErrorMessage).ToList();
+            throw new ErrorOnValidationException(errorMessages);
+        }
+    }
+}
