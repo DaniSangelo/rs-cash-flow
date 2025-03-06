c4c84568b4dbc178c6f04f40e90e6d72d9e6bdaf
Daniel Sângelo <danielsangelo@hotmail.com>
2025-03-06 20:46:21 -0300

    Apply OCP

--- src/CashFlow.API/Controllers/Filters/ExceptionFilter.cs
+++ src/CashFlow.API/Controllers/Filters/ExceptionFilter.cs
@@ -21,22 +21,10 @@ public class ExceptionFilter : IExceptionFilter
 
     private void HandleProjectException(ExceptionContext context)
     {
-        if (context.Exception is ErrorOnValidationException ex)
-        {
-            var errorResponse = new ResponseErrorJson(ex.Errors);
-            context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
-            context.Result = new BadRequestObjectResult(errorResponse);
-        } else if (context.Exception is NotFoundException nf)
-        {
-            var errorResponse = new ResponseErrorJson(nf.Message);
-            context.HttpContext.Response.StatusCode = StatusCodes.Status404NotFound;
-            context.Result = new BadRequestObjectResult(errorResponse);
-        } else
-        {
-            var errorResponse = new ResponseErrorJson(context.Exception.Message);
-            context.HttpContext. Response.StatusCode = StatusCodes.Status400BadRequest;
-            context.Result = new BadRequestObjectResult(errorResponse);
-        }
+        var cashFlowException = context.Exception as CashFlowException;
+        var errorResponse = new ResponseErrorJson(cashFlowException!.GetErrors());
+        context.HttpContext.Response.StatusCode = cashFlowException.StatusCode;
+        context.Result = new ObjectResult(errorResponse);
     }
 
     private void ThrowUnknowError(ExceptionContext context)
