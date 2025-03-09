d7fbc8131f7521b68c36d5f1971ab17af3898e1c
Daniel Sângelo <danielsangelo@hotmail.com>
2025-03-09 10:19:12 -0300

    Add entity user and migrations

--- src/CashFlow.Domain/Entities/Expense.cs
+++ src/CashFlow.Domain/Entities/Expense.cs
@@ -9,4 +9,7 @@ public class Expense
     public decimal Amount { get; set; }
     public DateTime Date { get; set; }
     public PaymentType PaymentType { get; set; }
+
+    public long UserId { get; set; }
+    public User User { get; set; } = default!; //the command "default!" says to .net that the property never will be null
 }
