dedd81693fea50ebf42b5cec89f3a925bdd3ed4a
Daniel Sângelo <danielsangelo@hotmail.com>
2025-03-15 10:27:52 -0300

    Add integration test for login

--- tests/WebApi.Test/CustomWebAppFactory.cs
+++ tests/WebApi.Test/CustomWebAppFactory.cs
@@ -1,4 +1,7 @@
-﻿using CashFlow.Infra.DataAccess;
+﻿using CashFlow.Domain.Entities;
+using CashFlow.Domain.Security.Cryptography;
+using CashFlow.Infra.DataAccess;
+using CommonTestUtilities.Entities;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
@@ -8,6 +11,8 @@ namespace WebApi.Test;
 
 public class CustomWebAppFactory : WebApplicationFactory<Program>
 {
+    private User _user;
+    private string _password;
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.UseEnvironment("Test")
@@ -19,6 +24,23 @@ public class CustomWebAppFactory : WebApplicationFactory<Program>
                     config.UseInMemoryDatabase("InMemoryDbForTesting");
                     config.UseInternalServiceProvider(provider);
                 });
+                var scope = services.BuildServiceProvider().CreateScope();
+                var dbContext = scope.ServiceProvider.GetRequiredService<CashFlowDbContext>();
+                var passwordEncrypter = scope.ServiceProvider.GetRequiredService<IPasswordEncrypter>();
+                StartDatabase(dbContext, passwordEncrypter);
             });
     }
+
+    private void StartDatabase(CashFlowDbContext dbContext, IPasswordEncrypter passwordEncrypter)
+    {
+        _user = UserBuilder.Build();
+        _password = _user.Password;
+        _user.Password = passwordEncrypter.Encrypt(_user.Password);
+        dbContext.Users.Add(_user);
+        dbContext.SaveChanges();
+    }
+
+    public string GetEmail() => _user.Email;
+    public string GetName() => _user.Name;
+    public string GetPassword() => _password;
 }
---
+++ tests/WebApi.Test/Login/LoginTest.cs
@@ -0,0 +1,60 @@
+﻿using CashFlow.Communication.Requests;
+using CashFlow.Exception;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+using System.Globalization;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
+using WebApi.Test.InLineData;
+
+namespace WebApi.Test.Login;
+
+public class LoginTest : IClassFixture<CustomWebAppFactory>
+{
+    private const string RESOURCE = "api/login";
+    private readonly HttpClient _httpClient;
+    private readonly string _email;
+    private readonly string _name;
+    private readonly string _password;
+
+    public LoginTest(CustomWebAppFactory webApp)
+    {
+        _httpClient = webApp.CreateClient();
+        _email = webApp.GetEmail();
+        _name = webApp.GetName();
+        _password = webApp.GetPassword();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var request = new RequestLoginJson
+        {
+            Email = _email,
+            Password = _password
+        };
+        var response = await _httpClient.PostAsJsonAsync(RESOURCE, request);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        var body = await response.Content.ReadAsStreamAsync();
+        var data = await JsonDocument.ParseAsync(body);
+        data.RootElement.GetProperty("name").GetString().Should().Be(_name);
+        data.RootElement.GetProperty("token").GetString().Should().NotBeNullOrEmpty();
+    }
+
+    [Theory]
+    [ClassData(typeof(CultureInLineDataTest))]
+    public async Task Error_Login_Invalid(string culture)
+    {
+        var request = RequestLoginJsonBuilder.Build();
+        _httpClient.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue(culture));
+        var response = await _httpClient.PostAsJsonAsync(RESOURCE, request);
+        response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        var responseBody = await response.Content.ReadAsStreamAsync();
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+        var errors = responseData.RootElement.GetProperty("errorMessage").EnumerateArray();
+        var expectedMessage = ResourceErrorMessages.ResourceManager.GetString("INVALID_CREDENTIALS", new CultureInfo(culture));
+        errors.Should().HaveCount(1).And.Contain(c => c.GetString()!.Equals(expectedMessage));
+    }
+}
