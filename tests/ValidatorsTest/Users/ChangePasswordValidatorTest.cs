6acfa1f73119ad3ec2c742e4580c37e59f05158a
Daniel Sângelo <danielsangelo@hotmail.com>
2025-03-16 08:50:37 -0300

    Add tests for change password

---
+++ tests/ValidatorsTest/Users/ChangePasswordValidatorTest.cs
@@ -0,0 +1,31 @@
+﻿using CashFlow.Application.UseCases.User;
+using CashFlow.Exception;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+using WebApi.Test.InLineData;
+
+namespace ValidatorsTest.Users;
+
+public class ChangePasswordValidatorTest
+{
+    [Fact]
+    public void Success()
+    {
+        var validator = new ChangePasswordValidator();
+        var request = RequestChangePasswordJsonBuilder.Build();
+        var result = validator.Validate(request);
+        result.IsValid.Should().BeTrue();
+    }
+
+    [Theory]
+    [ClassData(typeof(EmptyOrNullInLineDataTest))]
+    public void Error_NewPassword_Empty(string newPassword)
+    {
+        var validator = new ChangePasswordValidator();
+        var request = RequestChangePasswordJsonBuilder.Build();
+        request.NewPassword = newPassword;
+        var result = validator.Validate(request);
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.INVALID_PASSWORD));
+    }
+}
