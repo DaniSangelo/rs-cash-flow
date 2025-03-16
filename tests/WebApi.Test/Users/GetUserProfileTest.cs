2cd1deaa8ab5b0866b0340479b0973ea1a39fc37
Daniel Sângelo <danielsangelo@hotmail.com>
2025-03-15 21:28:08 -0300

    Add tests for get user profile

---
+++ tests/WebApi.Test/Users/GetUserProfileTest.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+
+namespace WebApi.Test.Users;
+
+public class GetUserProfileTest : CashFlowClassFixture
+{
+    private const string RESOURCE = "api/User";
+
+    private readonly string _token;
+    private readonly string _userName;
+    private readonly string _userEmail;
+
+    public GetUserProfileTest(CustomWebAppFactory webApp) : base(webApp)
+    {
+        _token = webApp.User_Team_Member.GetToken();
+        _userName = webApp.User_Team_Member.GetName();
+        _userEmail = webApp.User_Team_Member.GetEmail();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var result = await SendGet(RESOURCE, _token);
+        result.StatusCode.Should().Be(HttpStatusCode.OK);
+        var body = await result.Content.ReadAsStreamAsync();
+        var response = await JsonDocument.ParseAsync(body);
+        response.RootElement.GetProperty("name").GetString().Should().Be(_userName);
+        response.RootElement.GetProperty("email").GetString().Should().Be(_userEmail);
+    }
+}
